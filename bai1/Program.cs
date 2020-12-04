165237520f11efd97dabe68edb9a7fc1c53e19a8
unknown <you@example.com>
2020-12-04 15:39:50 +0700

    thuc hanh kiem tra so nguyen to

---
+++ bai1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if(number<2){
+                Console.WriteLine($"{number} is not a prime");
+            }
+            else{
+                bool check = true;
+                for(int i =2;i<Math.Sqrt(number);i++){
+                    if(number%i==0){
+                        check = false;
+                        break;
+                    }
+                }
+                if(check){
+                    Console.WriteLine($"{number} is a prime");
+                }
+                else{
+                    Console.WriteLine($"{number} is not a prime");
+                }
+            }
+        }
+    }
+}
