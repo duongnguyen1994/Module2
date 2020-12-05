40b9a4efc27519c5d592f9c8e61d9f73c692dfb8
unknown <you@example.com>
2020-12-05 20:40:57 +0700

    day of month

---
+++ bai1/dayofmonth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace dayofmonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a month: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            switch (month) 
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12: Console.WriteLine("This month has 31 days");
+                break;
+                case 2: Console.WriteLine("This month has 28 or 29 days");
+                break;
+                case 4:
+                case 6:
+                case 9:
+                case 11: Console.WriteLine("This month has 30 days");
+                break;
+                default: Console.WriteLine($"{month} is not a month of year");
+                break;
+            }
+        }
+    }
+}
