6679c255b2dda03ffcbbdb5a5cf4e52d23538cc4
unknown <you@example.com>
2020-12-05 20:31:06 +0700

    UCLN

---
+++ bai1/UCLN/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UCLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if(a == 0 && b ==0)
+            {
+                Console.WriteLine("No greatest common factor");
+            }else if(a ==0 || b ==0)
+            {
+                Console.WriteLine($"Greatest common factor: {a+b}");
+            }else if (a>b)
+            {
+                Console.WriteLine($"Greatest common factor: {a-b}");
+            }
+            else if(a<b)
+            {
+                Console.WriteLine($"Greatest common factor: {b-a}");
+            }
+            else
+            {
+                Console.WriteLine($"Greatest common factor: {a}");
+            }
+        }   
+    }
+}
