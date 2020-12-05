7f7b2c22ec2fc030119216aad635fc060c3639a0
unknown <you@example.com>
2020-12-05 23:11:52 +0700

    add variable data type

---
+++ bai1/VariableDataType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariableDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float f = 20.5f;
+            double d = 20.5;
+            bool b = true;
+            char c = 'a';
+            string s = "CodeGym";
+            Console.WriteLine($"Giá trị của biến i là: {i}");
+            Console.WriteLine($"Giá trị của biến i là: {f}");
+            Console.WriteLine($"Giá trị của biến i là: {d}");
+            Console.WriteLine($"Giá trị của biến i là: {b}");
+            Console.WriteLine($"Giá trị của biến i là: {c}");
+            Console.WriteLine($"Giá trị của biến i là: {s}");
+        }
+    }
+}
