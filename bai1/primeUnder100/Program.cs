556e942ccd9e1e8033749e0b25c5320d5d2fa3f1
unknown <you@example.com>
2020-12-06 21:58:28 +0700

    Primes under 100

---
+++ bai1/primeUnder100/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace primeUnder100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            bool check = true;
+            while(number<100)
+            {
+                for(int i=2;i<number;i++)
+                {
+                    if(number%i ==0)
+                    {
+                        check =false;
+                        break;
+                    }
+                }
+                if(check)
+                {
+                    Console.Write($"{number} ");
+                }
+                check = true;
+                number++;
+            }
+        }
+    }
+}
