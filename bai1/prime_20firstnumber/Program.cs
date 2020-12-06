36afad8fb983ac16780efa33a7973ce4947d5c44
unknown <you@example.com>
2020-12-06 14:38:53 +0700

    20 first number prime

---
+++ bai1/prime_20firstnumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prime_20firstnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int number = 2;
+            bool check = true;
+            while(count<20)
+            {
+                for(int i =2;i<number;i++)
+                {
+                    if(number%i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if(check == true)
+                {
+                    Console.Write($"{number} ");
+                    count++;
+                }
+                check = true;
+                number++;
+            }
+        }
+    }
+}
