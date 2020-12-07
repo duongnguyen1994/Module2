6d2f693cfe2dcc29079a4ee7c4479f0b614ecd9c
unknown <you@example.com>
2020-12-07 11:24:45 +0700

    su dung array

---
+++ bai2/findValueInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace findValueInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+            Console.WriteLine("Enter a name's student: ");
+            string name = Console.ReadLine();
+            name = toUpperCase(name);
+            for(int i=0; i<students.Length ;i++)
+            {
+                if(name == students[i])
+                {
+                    Console.WriteLine($"Position of {name} in list is {i+1}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{name} is not in this list");
+                    break;
+                }
+            }
+        }
+        static string toUpperCase(string input)
+        {
+            char[] letter = input.ToCharArray();
+            letter[0] = char.ToUpper(letter[0]);
+            return new string(letter);
+        }
+    }
+}
---
+++ bai2/useArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace useArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int total = 0;
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            for(int i = 0;i<numbers.Length;i++)
+            {
+                total += numbers[i];
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine($"Total = {total}");
+        }
+    }
+}
