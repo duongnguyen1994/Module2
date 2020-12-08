30396d092d93c55dc2c3b10bde1c1d2484da15d2
unknown <you@example.com>
2020-12-08 14:21:00 +0700

    count character of string

---
+++ bai2/countCharacterInString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace countCharacterInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            char character;
+            bool check = true;
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Enter a character: ");
+                check = char.TryParse(Console.ReadLine(), out character);
+            } while (!check);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == character)
+                {
+                    count++;
+                }
+            }
+            if(character ==' ')
+            {
+                Console.WriteLine($"{str} has {count} non-breaking space"); 
+            } 
+            else     
+            Console.WriteLine($"{str} has {count} {character}");          
+        }
+    }
+}
