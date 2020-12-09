b25029a47f7d5979ca4681d57ad2ca56264acd4e
nguyen.trduong1994@gmail.com <nguyen.trduong1994@gmail.com>
2020-12-09 08:49:19 +0700

    Rectangle

---
+++ bai3/Rectangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rectangle
+{
+    public class Rectangle
+    {
+        double width,height;
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+        public double GetPerimeter()
+        {
+            return (this.width+this.height)*2;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Rectangle:\nwidth = {this.width}, height = {this.height}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter width:");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height:");
+            double height = double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width,height);
+            rectangle.Display();
+            Console.WriteLine($"Area = {rectangle.GetArea()}");
+            Console.WriteLine($"Perimeter = {rectangle.GetPerimeter()}");
+        }
+    }
+}
