f715911f6291b56c03204121a30dc8a726421a3b
nguyen.trduong1994@gmail.com <nguyen.trduong1994@gmail.com>
2020-12-13 23:59:18 +0700

    thuc hanh Access Modifier

---
+++ bai4/ThAccessModifier/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicLib.Class1 pc = new PublicLib.Class1();
+            int sum = pc.Sum(1,1);
+
+            InternalLib.Class1 ic = new InternalLib.Class1();
+            sum = ic.Sum(2,2);
+        }
+    }
+}
